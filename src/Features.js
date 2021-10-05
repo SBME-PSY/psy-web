1c79a1aabba9602ceec253d18c13bdeeadfb3b2f
Omar_Mohamed_1 <omar.abdel-aal00@eng-st.cu.edu.eg>
2021-10-05 11:28:48 +0200

    added contact us link

--- src/Features.js
+++ src/Features.js
@@ -1,7 +1,7 @@
 import React from "react"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Container, Row } from "reactstrap";
+import { Button, Col, Container, Row } from "reactstrap";
 import { Link } from "react-router-dom";
 import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,7 +12,7 @@ const Features=({features})=>{
     return(
         
         <Container id="Features" className="my-5">
-            <Link to="/src/Contactus.js" target="_blank" rel="noopener noreferrer">Contact Us <FontAwesomeIcon icon={faMailBulk}/></Link>
+            <Button className="btn btn-primary rounded-pill m-5 position-fixed bottom-0 end-0"><Link className="text-decoration-none text-white" to="/src/Contactus.js" target="_blank" rel="noopener noreferrer">Contact Us <FontAwesomeIcon icon={faMailBulk}/></Link></Button>
             <h1 id="feature-header" className="text-center mb-5">Through Psy-Awareness You can do the following:</h1>
             <Row className="mt-5 d-flex justify-content-center">
             {features.map((item,id_name,color_class)=> {
@@ -25,7 +25,7 @@ const Features=({features})=>{
                     id_name = "feature-odd";
                 };
                 return(
-                <Col xs={12} md={6} lg={4} key={item.id} id={id_name} className="mt-4 mx-2">
+                <Col xs={12} md={6} lg={4} key={item.id} id={id_name} className="mt-4 mx-3">
                     <img width="250px" height="200px" className="mt-5" src={item.imagesrc} alt='group text chat'></img>
                     <p width="40%" className={color_class.concat(" text-center pt-5 mt-5")} >{item.caption}</p>
                 </Col>

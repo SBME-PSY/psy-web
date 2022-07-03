82af79fc819502c8fee0b8de6e1b3af1289aada5
ahmed galal <ahmedgabdelaziz4@gmail.com>
2022-07-03 16:22:43 +0200

    Article added

--- src/Screens/Article.js
+++ src/Screens/Article.js
@@ -1,10 +1,73 @@
-import React from 'react';
-import styled from 'styled-components';
-function Article(props) {
-  return <Wrapper>Galal put Your article here</Wrapper>;
+import React from "react";
+import { useState } from "react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import {CKEditor} from "@ckeditor/ckeditor5-react"
+import { Button, Container,Input } from "reactstrap";
+import sky from '../assets/Img/sky.jpg'
+import { useSelector } from 'react-redux';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+function Article() {
+  
+  const userId = useSelector((state) => state.user._id);
+  const [text, setText] = useState("")
+  const [title, setTitle] = useState("")
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+
+  
+
+    const handleclick = () => {
+      axios({
+        method: 'POST',
+        url: '/psy/articles',
+        data: {
+          "title": title,
+          "body": text,
+          "author": userId
+
+        },
+      })
+        .then((res) => {
+          console.log(res.data);
+          navigate('/');
+        })
+        .catch((err) => {
+          console.log(err.response.data.message);
+          dispatch({ type: 'UPDATE_ERR', pyload: err.response.data.message });
+        });
+    }
+
+  return (
+    <div id='my-article' className='d-flex flex-1'  style={{ 
+      backgroundImage: `url(${sky})`
+    }}>
+    <Container id="Article" >
+        <div id="headr" className="d-flex justify-content-center " >
+        <Input type="text" name="" placeholder="title" value={title} onChange={(e) =>{setTitle(e.target.value)
+        console.log(title)
+        }}/>
+
+        </div>
+        
+      <div className="editor">
+        <CKEditor
+          editor={ClassicEditor}
+          data={text}
+          onChange={(event, editor) => {
+            const data = editor.getData()
+            setText(data)
+          }}
+        />
+      </div>
+      
+      <Button color="success" className='mt-3 w-25 rounded-pill ' onClick={handleclick} >POST</Button>
+    </Container>
+    </div>
+  )
 }
 
-const Wrapper = styled.div`
-  text-align: center;
-`;
-export default Article;
+export  default Article;
--- src/context.js
+++ src/context.js
@@ -31,80 +31,80 @@ export function AppProvider({ children }) {
     const videoGrid = document.getElementById('video-grid');
     videoGrid.appendChild(remoteVideo);
   };
-  useEffect(() => {
-    const socket = io('ws://localhost:3001');
-    const peer = new Peer(undefined, {
-      host: '/',
-      port: '9000',
-      path: '/myapp',
-    });
-    peer.on('open', function (id) {
-      store.dispatch({ type: 'UPDATE_PEER', pyload: peer });
-    });
-    peer.on('call', (call) => {
-      const { stream } = store.getState();
-      console.log(stream);
-      const remoteVideo = document.createElement('video');
+  // useEffect(() => {
+  //   const socket = io('ws://localhost:3001');
+  //   const peer = new Peer(undefined, {
+  //     host: '/',
+  //     port: '9000',
+  //     path: '/myapp',
+  //   });
+  //   peer.on('open', function (id) {
+  //     store.dispatch({ type: 'UPDATE_PEER', pyload: peer });
+  //   });
+  //   peer.on('call', (call) => {
+  //     const { stream } = store.getState();
+  //     console.log(stream);
+  //     const remoteVideo = document.createElement('video');
 
-      call.on('stream', function (remoteStream) {
-        addvideoStream(remoteStream, remoteVideo);
-      });
-      call.answer(stream);
-      store.dispatch({
-        type: 'UPDATE_PEERS',
-        pyload: { peerId: call.peer, call },
-      });
-      call.on('close', () => {
-        remoteVideo.remove();
-      });
-    });
-    peer.on('connection', function (conn) {
-      conn.on('data', function (data) {
-        conn.send('i recieved' + data);
-      });
-    });
-    socket.on('connect', () => {
-      store.dispatch({ type: 'UPDATE_SOCKET', pyload: socket });
-    });
-    // socket.on('user-disconnect', (peerId) => {
-    //   console.log(peerId);
-    //   const { peers } = store.getState();
-    //   if (peers[peerId]) {
-    //     peers[peerId].close();
-    //   }
-    // });
+  //     call.on('stream', function (remoteStream) {
+  //       addvideoStream(remoteStream, remoteVideo);
+  //     });
+  //     call.answer(stream);
+  //     store.dispatch({
+  //       type: 'UPDATE_PEERS',
+  //       pyload: { peerId: call.peer, call },
+  //     });
+  //     call.on('close', () => {
+  //       remoteVideo.remove();
+  //     });
+  //   });
+  //   peer.on('connection', function (conn) {
+  //     conn.on('data', function (data) {
+  //       conn.send('i recieved' + data);
+  //     });
+  //   });
+  //   socket.on('connect', () => {
+  //     store.dispatch({ type: 'UPDATE_SOCKET', pyload: socket });
+  //   });
+  //   // socket.on('user-disconnect', (peerId) => {
+  //   //   console.log(peerId);
+  //   //   const { peers } = store.getState();
+  //   //   if (peers[peerId]) {
+  //   //     peers[peerId].close();
+  //   //   }
+  //   // });
 
-    socket.on('user-connected', (userId) => {
-      const { stream } = store.getState();
-      let call = peer.call(userId, stream);
-      const remoteVideo = document.createElement('video');
-      call.on('stream', function (remoteStream) {
-        addvideoStream(remoteStream, remoteVideo);
-      });
-      store.dispatch({
-        type: 'UPDATE_PEERS',
-        pyload: { peerId: call.peer, call },
-      });
-      call.on('close', () => {
-        remoteVideo.remove();
-      });
-      const conn = peer.connect(userId);
-      conn.on('open', () => {
-        conn.send('hi!');
-      });
-      conn.on('data', function (data) {});
-    });
-    // socket.on('no-room', (roomId) => {
-    //   console.log('no room id = ' + roomId);
-    //   store.dispatch({
-    //     type: 'UPDATE_ALERT',
-    //     pyload: { isNew: true, message: `not valid Id ${roomId}` },
-    //   });
-    // });
-    return () => {
-      socket.close();
-    };
-  }, [store]);
+  //   socket.on('user-connected', (userId) => {
+  //     const { stream } = store.getState();
+  //     let call = peer.call(userId, stream);
+  //     const remoteVideo = document.createElement('video');
+  //     call.on('stream', function (remoteStream) {
+  //       addvideoStream(remoteStream, remoteVideo);
+  //     });
+  //     store.dispatch({
+  //       type: 'UPDATE_PEERS',
+  //       pyload: { peerId: call.peer, call },
+  //     });
+  //     call.on('close', () => {
+  //       remoteVideo.remove();
+  //     });
+  //     const conn = peer.connect(userId);
+  //     conn.on('open', () => {
+  //       conn.send('hi!');
+  //     });
+  //     conn.on('data', function (data) {});
+  //   });
+  //   // socket.on('no-room', (roomId) => {
+  //   //   console.log('no room id = ' + roomId);
+  //   //   store.dispatch({
+  //   //     type: 'UPDATE_ALERT',
+  //   //     pyload: { isNew: true, message: `not valid Id ${roomId}` },
+  //   //   });
+  //   // });
+  //   return () => {
+  //     socket.close();
+  //   };
+  // }, [store]);
 
   useEffect(() => {
     const jwt = getCookie('jwt');
